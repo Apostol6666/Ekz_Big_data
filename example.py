a60fb4133cc87bc1aef282ebe0ebc0a8d99d70e1
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2020-05-26 02:39:36 +0800

    Update example.py

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ from keras.layers import Dense, Embedding, Flatten
 from keras import utils
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
-from keras.callbacks import ModelCheckpoint
 from pymongo import MongoClient
+
 import numpy as np
 
 client = MongoClient()
@@ -15,16 +15,20 @@ num_words = 10000
 
 xs = []
 ys = []
+x_test = []
+x_test_id = []
 
 ycount=collection.count({"y": {"$exists": "true"}})
 
 for k in range(ycount):
     xs.append([]) 
+    x_test.append([])
 
 yi = 0
 N=20
 
-for everyy in collection.find({"y": {"$exists": "true"}}):
+for everyy in collection.find({"y": {"$exists": "true"}}):  
+    x_test_id.append(everyy["_id"])
     i=0
     ys.append(everyy["y"])
     xname = "x" + str(i)
@@ -36,27 +40,50 @@ for everyy in collection.find({"y": {"$exists": "true"}}):
         i+=1
     yi+=1
 
+yi=0 
+
+for everynoy in collection.find({"y":{"$exists":"false"}}):
+    i=0
+    xname = "x" + str(i) 
+    while i!=N:
+        xname = "x" + str(i) 
+        everyx = everyy[xname]
+        x_test[yi].append(everyx)
+        i=int(i)
+        i+=1
+    yi+=1
+ 
+
 tokenizer = Tokenizer(num_words=num_words)
 tokenizer.fit_on_texts(ys)
 tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(ys)
-y_train = pad_sequences(sequences, 20)
+y_train = pad_sequences(sequences)
 
-x_train = np.zeros(len(xs[0]), len(xs), 1)
+x_train = xs
 
 model = Sequential()
 model.add(Embedding(num_words, 64, input_length = 20))
-model.add(Dense(128, activation='relu',input_shape = (x_train.shape, )))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-model.fit(x_train, y_train, epochs=20, batch_size=1, verbose=2)
-
-mse, mae = model.evaluate(x_test, y_test, verbose=0)
-
-print("Средняя абсолютная ошибка (тысяч долларов):", mae)
+model.fit(x_train, y_train, epochs=5, batch_size=1, verbose=2)
 
 pred = model.predict(x_test)
+print(tokenizer.word_index[pred[0]])
+print(pred[1][0])
+ 
 
-print("Предсказанная стоимость:", pred[1][0], ", правильная стоимость:", y_test[1])
+for id in x_test_id:
+    item = collection.find({"_id":id})
+    print(item)
+
+plt.plot(history.history['accuracy'], 
+         label='Доля верных ответов на обучающем наборе')
+plt.xlabel('Эпоха обучения')
+plt.ylabel('Доля верных ответов')
+plt.legend()
+plt.show()
